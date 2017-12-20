d4561f2c0530c59be62eadf3828922859acb4f74
zhengss <zhengss332@gmail.com>
2017-12-20 15:48:18 +0800

    chat socket

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const mime = require('mime');
+const chatServer = require('./lib/chat_server');
 let cache = {};
 
 // 创建HTTP服务器处理每个请求
 const server = http.createServer((request, response) => {
   let filePath = '';
 
-  console.log(request.url);
   if (request.url == '/') {
     filePath = 'public/index.html';
   } else {
@@ -22,6 +22,11 @@ const server = http.createServer((request, response) => {
 server.listen(3000, () => {
   console.log('Server listen listening on http://localhost:3000');
 });
+
+// 启动Socket.IO服务器, 并与HTTP共享同一个TCP/IP端口
+chatServer.listen(server);
+
+
 
 function send404(response) {
   response.writeHead(404, { 'Content-Type': 'text/plain' });
